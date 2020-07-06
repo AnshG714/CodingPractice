a89ff76cf8d1d1364a7a60cb4b8475775a4eb9f5
Ansh Godha <ag759@cornell.edu>
2020-07-06 21:21:05 +0530

    Tree Serialization and Deserialization done

--- Trees/serializeDeserialize.py
+++ Trees/serializeDeserialize.py
@@ -1,6 +1,72 @@
+from collections import deque
+from Tree import *
+
+
 def serialize(root):
-    pass
+    if not root:
+        return '[]'
+
+    res = [root.val]
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+
+        size = len(queue)
+
+        for _ in range(size):
+            top = queue.popleft()
+            if top.left:
+                queue.append(top.left)
+                res.append(top.left.val)
+            else:
+                res.append(None)
+
+            if top.right:
+                queue.append(top.right)
+                res.append(top.right.val)
+            else:
+                res.append(None)
+
+    # trim Nones from the end
+    p = len(res) - 1
+    while res[p] == None:
+        p -= 1
+
+    return str(res[:p + 1])
 
 
 def deserialize(string):
-    pass
+    if string == '[]':
+        return None
+
+    listEls = stringListToList(string)
+
+    i = 1
+    queue = deque()
+    root = TreeNode(listEls[0])
+    queue.append(root)
+    while i < len(listEls):
+
+        top = queue.popleft()
+
+        if listEls[i] != "None":
+            top.left = TreeNode(listEls[i])
+            queue.append(top.left)
+
+        i += 1
+
+        if i >= len(listEls):
+            break
+
+        if listEls[i] != "None":
+            top.right = TreeNode(listEls[i])
+            queue.append(top.right)
+
+        i += 1
+
+    return root
+
+
+def stringListToList(string: str):
+    return string.strip("[]").replace(' ', '').split(',')
