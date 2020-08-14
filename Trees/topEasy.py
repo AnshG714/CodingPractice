c36d96f0a67b59ffd454a770a74380b7c0d02995
Ansh Godha <ag759@cornell.edu>
2020-08-14 23:07:17 +0530

    Add trimBST

--- Trees/topEasy.py
+++ Trees/topEasy.py
@@ -106,3 +106,20 @@ def isMirror(root1, root2):
 
 def isSymmetric(root):
     return isMirror(root, root)
+
+
+def trimBST(root, L, R):
+    """
+    Given a binary search tree and the lowest and highest boundaries as L and R, 
+    trim the tree so that all its elements lies in [L, R] (R >= L). You might 
+    need to change the root of the tree, so the result should return the new 
+    root of the trimmed binary search tree.
+    """
+    if L <= root.val <= R:
+        root.left = trimBST(root.left, L, R)
+        root.right = trimBST(root.right, L, R)
+        return root
+    elif root.val < L:
+        return trimBST(root.right, L, R)
+    else:
+        return trimBST(root.left, L, R)
